6e5a6c6748d4cc4b3a0522a47378773f9ad99dd8
Rodrigo930708 <161263809+Rodrigo930708@users.noreply.github.com>
2024-02-25 19:06:09 -0600

    feat: Add project with basic configuration in spring boot

---
+++ com.demo/src/main/java/com/demo/SpringBootH2Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@PropertySource("classpath:application-h2.properties")
+public class SpringBootH2Application {
+
+    public static void main(String... args) {
+        SpringApplication.run(SpringBootH2Application.class, args);
+    }
+}
